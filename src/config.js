3a799ad51e732e575e7755fae2d298404cfb5f71
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-09-21 13:10:00 -0400

    put server in dev

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: 'development',
     DB_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/meal-planner",
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
